327ee9b8bf1e4965179b1c27ec27b4475bc27bbd
Hoang Long <hoanglongmcs1@gmail.com>
2020-03-11 17:26:27 +0700

    Calendar ver-2.0

--- js/calendar.js
+++ js/calendar.js
@@ -1,26 +1,36 @@
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'
 ];
+const weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
 const prevBtn = document.querySelector(".prev");
 const nextBtn = document.querySelector(".next");
 const currentMonthDisplay = document.querySelector("#month");
 const dateStrDisplay = document.querySelector("#date_str");
-const calendarDisplay = document.querySelector(".calendar-days");
+const dayDisplay = document.querySelector(".calendar-days");
+const weekdayDisplay = document.querySelector(".calendar-weekdays");
 
 let dt = new Date(); // take your system datetime
-let today = new Date();
+const today = new Date();
+dt.setDate(1);
 
+const renderHeader = () => {
+    currentMonthDisplay.innerHTML = months[dt.getMonth()];
+    dateStrDisplay.innerHTML = dt.toDateString();
+}
+const renderWeekDays = () => {
+    let cells = "";
+    for (let i = 0; i < 7; i++) {
+        cells += `<div>${weekdays[i]}</div>`;
+    }
+    weekdayDisplay.innerHTML = cells;
+}
 const renderDate = () => {
-    dt.setDate(1);
     let dayBegin = dt.getDay(); // take day of week
     let endDate = new Date(dt.getFullYear(), dt.getMonth() + 1, 0).getDate(); // take number of date of current month
     let prevDate = new Date(dt.getFullYear(), dt.getMonth(), 0).getDate();
-
-    dateStrDisplay.innerHTML = dt.toDateString();
-    currentMonthDisplay.innerHTML = months[dt.getMonth()];
-
     let cells = "";
-    for (x = dayBegin; x > 0; x--) {
-        cells += `<div class="prev-date">${prevDate - x + 1}</div>`;
+    for (let i = dayBegin; i > 0; i--) {
+        cells += `<div class="prev-date">${prevDate - i + 1}</div>`;
     }
     for (let day = 1; day <= endDate; day++) {
         if (day === today.getDate() && today.getMonth() === dt.getMonth()) {
@@ -29,21 +39,25 @@ const renderDate = () => {
             cells += `<div>${day}</div>`;
         }
     }
-    calendarDisplay.innerHTML = cells;
+    dayDisplay.innerHTML = cells;
 }
 
 const moveBack = () => {
     dt.setMonth(dt.getMonth() - 1);
+    renderHeader();
     renderDate();
 }
 const moveNext = () => {
     dt.setMonth(dt.getMonth() + 1);
+    renderHeader();
     renderDate();
 }
 
 const init = () => {
     prevBtn.addEventListener("click", moveBack);
     nextBtn.addEventListener("click", moveNext);
+    renderHeader();
+    renderWeekDays();
     renderDate();
 }
 
